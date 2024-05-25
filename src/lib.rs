372616901f71b6910eefaee4c90828041a07cfe9
deref0ptr <158045356+deref0ptr@users.noreply.github.com>
2024-05-25 11:26:32 +0100

    Add documentation and improve formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,53 @@
+//! Provides the [lazytest] macro to reduce the amount of code needed to write unit tests.
+
+/// This macro converts blocks of the form:
+/// ```ignore
+///     test_name {
+///         test_body
+///     }
+/// ```
+/// into unit test functions and places them in a `#[cfg(test)]` module.
+/// 
+/// Note:
+/// - The tests are automatically marked with `#[test]`. Other attributes (e.g. `#[ignore]`) can be added as usual above each test block.
+/// - The test module includes a `use super::*;` import.
+/// - Only `()` return types are currently supported.
 #[macro_export]
 macro_rules! lazytest {
-	(
-		$(	$(#[$attr:meta])*
-			$test_name:ident
-				$body2:block
-		)*
-	) => {
-		#[cfg(test)]
-		mod tests {
-			#[allow(unused_imports)]
-			use super::*;
-			
-			$(
-				$(#[$attr])*
-				#[test]
-				fn $test_name()
-					$body2
-			)*
-		}
-	};
+    (
+        $(
+            $(#[$attr:meta])*
+            $test_name:ident
+                $body:block
+        )*
+    ) => {
+        #[cfg(test)]
+        mod tests {
+            #[allow(unused_imports)]
+            use super::*;
+
+            $(
+                $(#[$attr])*
+                #[test]
+                fn $test_name()
+                    $body
+            )*
+        }
+    };
 }
 
 lazytest! {
 
-	// Basic use
-	identity {
-		assert_eq!(1, 1);
-	}
+    // Basic use
+    identity {
+        assert_eq!(1, 1);
+    }
 
-	// With an attribute
-	#[ignore]
-	sine {
-		let sin_half_pi = std::f64::consts::FRAC_PI_2.sin();
-		assert!((sin_half_pi - 1.0).abs() < 1e-10);
-	}
+    // With an attribute
+    #[ignore]
+    sine {
+        let sin_half_pi = std::f64::consts::FRAC_PI_2.sin();
+        assert!((sin_half_pi - 1.0).abs() < 1e-10);
+    }
 
 }
